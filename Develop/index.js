74ba7aae591d27f53c11182093e81db9f0de2120
Sam Bonias <sbonias84@gmail.com>
2020-05-28 14:37:35 -0400

    coded fs.writeFile to run after .prompt

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,17 @@
 // The generated README includes 1 badge that's specific to the repository.
 
 // -------Require--------
+// set the standard library for reading and writing files
 const fs = require("fs");
 // https://www.npmjs.com/package/inquirer
 const inquirer = require("inquirer");
 // https://www.npmjs.com/package/dotenv
 const dotenv = require("dotenv").config();
+// referencing the generateMarkdown module to allow for import
+const generateMarkdown = require("./utils/generateMarkdown.js");
+console.log(generateMarkdown);
 
-// -------Establishment of Questions using Inquirer syntax--------
+// -------Function that initializes the question asking process--------
 // should rewrite as a function to apply inquirer.prompt
 // incorporate data validation?
 // this is going to be an array of questions based on readme criteria
@@ -25,70 +29,77 @@ const dotenv = require("dotenv").config();
 // What are your questions? = [array: string]: ["How do I install this application?"]
 // Provide the link to your User Github profile picture. [string]
 // Provide your User Github email address. [string]
-const questions = [
-  inquirer.prompt([
-    { type: "input", name: "title", message: "What is your Project Title?" },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a Description of your project.",
-    },
-    {
-      type: "input",
-      name: "tableContents",
-      message: "List out your table of contents.",
-    },
-    {
-      type: "input",
-      name: "install",
-      message: "Provide installation requirements",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Provide usage instructions",
-    },
-    {
-      type: "input",
-      name: "license",
-      message: "Provide any license requirements.",
-    },
-    {
-      type: "input",
-      name: "contributors",
-      message: "Provide your contributors",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "What tests did you apply to the project?",
-    },
-    {
-      type: "input",
-      name: "questions",
-      message: "List out your questions",
-    },
-    {
-      type: "input",
-      name: "githubImage",
-      message: "Provide a link to your Github profile picture",
-    },
-    {
-      type: "input",
-      name: "githubUserName",
-      message: "Provide your Github e-mail address",
-    },
-  ]),
-];
-
-// -------Function that writes to the readme.md file--------
-function writeToFile(fileName, data) {
-  // determine location of where file should be placed
-  // create a file with name fileName
-  // write to file fileName the data
+//const questions = []
+function promptUser() {
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is your Project Title?",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Provide a Description of your project.",
+        },
+        {
+          type: "input",
+          name: "tableContents",
+          message: "List out your table of contents.",
+        },
+        {
+          type: "input",
+          name: "install",
+          message: "Provide installation requirements",
+        },
+        {
+          type: "input",
+          name: "usage",
+          message: "Provide usage instructions",
+        },
+        {
+          type: "input",
+          name: "license",
+          message: "Provide any license requirements.",
+        },
+        {
+          type: "input",
+          name: "contributors",
+          message: "Provide your contributors",
+        },
+        {
+          type: "input",
+          name: "tests",
+          message: "What tests did you apply to this project?",
+        },
+        {
+          type: "input",
+          name: "githubImage",
+          message: "Provide a link to your Github profile picture",
+        },
+        {
+          type: "input",
+          name: "githubUserName",
+          message: "Provide your Github e-mail address",
+        },
+      ])
+      // -------fs.writeFile--------
+      // placed here so it occurs after the ".prompt" finishes executing
+      // determine location of where file should be placed
+      // create a file with name fileName = "README.md"
+      // write to file fileName the data = "generateMarkdown(data)"
+      .then((data) => {
+        fs.writeFile("./utils/README.md", generateMarkdown(data), function (
+          err
+        ) {
+          if (err) {
+            return console.log(err);
+          }
+          return console.log("Success!");
+        });
+      })
+  );
 }
-
-// -------Function that initializes the question asking process--------
-function init() {}
-
-init();
+promptUser();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,16 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
+# ${data.description}
+# ${data.tableContents}
+# ${data.install}
+# ${data.usage}
+# ${data.license}
+# ${data.contributors}
+# ${data.tests}
+# ${data.githubImage}
+# ${data.githubUserName}
 `;
 }
-
+// module.exports is an object we use to store variables or methods and allows for export
 module.exports = generateMarkdown;
